fe7a102eb24205397b3c4d3fac89d9168e714b95
VikaMin <4673813@gmail.com>
2023-01-15 22:27:48 +0300

    commit v1

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/slxnrd8u9sc97o23/branch/main?svg=true)](https://ci.appveyor.com/project/VikaMin/web/branch/main)
---
+++ src/test/java/OrderTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OrderTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+   static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+        driver = null;
+
+    }
+
+    @Test
+    public void successfulForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров-Водкин Петр");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79107852369");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+    }
+}
